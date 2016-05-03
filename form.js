3f779e8e84d79e2df141f42607edb8a699ea5c2f
Mike San Román <mike@msanroman.io>
2016-05-03 21:43:47 +0200

    Adds validation for name, username and email

--- form.js
+++ form.js
@@ -6,11 +6,45 @@ function validateEmail(sEmail) {
 				return false;
 }
 
+function validaNombre() {
+	var nombre = $('input[name="personal"]').val();
+	if (nombre.length < 4 || nombre.length > 15) { //Incorrecto
+		$('input[name="personal"]').prev().show();
+	} else $('input[name="personal"]').prev().hide();
+}
+
+function validaUsername() {
+	var username = $('input[name="username"]').val();
+	if (username.trim().includes(' ') || username.length == 0) {
+		$('input[name="username"]').prev().show();
+	} else $('input[name="username"]').prev().hide();
+}
+
+function validaEmail() {
+	var email = $('input[name="email"]').val();
+	if (validateEmail(email)) {
+		$('input[name="email"]').prev().hide();
+	} else $('input[name="email"]').prev().show();
+}
+
+$('input[name="personal"]').focusout(function() {
+	validaNombre();
+});
+
+$('input[name="username"]').focusout(function() {
+	validaUsername();
+});
+
+$('input[name="email"]').focusout(function() {
+	validaEmail();
+});
+
 $('form').submit(function(ev) {
+	validaNombre();
+	validaUsername();
+	validaEmail();
+	if($('.error:visible').length > 0) {
 		ev.preventDefault();
-		var nombre = $('input[name="personal"]').val();
-		if (nombre.length < 4 || nombre.length > 15) { //Incorrecto
-			$('input[name="personal"]').prev().show();
-		}
+	}
 });
 
